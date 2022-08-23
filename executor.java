c868a22d1ed2fc42f47c820691d3c862746f3a0d
Raj Gautam <98406784+Rajgautam20@users.noreply.github.com>
2022-08-23 18:42:29 +0530

    Add files via upload

--- executor.java
+++ executor.java
@@ -1,3 +1,6 @@
+//In the above program, we are creating a fixed-size thread pool of 5 runnable threads. Then we are submitting 10 jobs to this pool, since the pool size is 5, it will start working on 5 jobs and other jobs will be in wait state, as soon as one of the job is finished, another job from the wait queue will be picked up by runnable thread and get’s executed. Here is the output of the above program.
+
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -44,3 +47,5 @@ public class Main {
         System.out.println("Finished all threads");
     }
 }
+
+//The output confirms that there are five threads in the pool named from “pool-1-thread-1” to “pool-1-thread-5” and they are responsible to execute the submitted tasks to the pool.
